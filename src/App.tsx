c522a43bd4641c4ac9336b651187f1bb807be80b
maurizio <maurizio@pop-os.station>
2026-04-16 14:07:38 +0200

    v1.0.6

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 
 // Get version - this will be replaced during build
-const APP_VERSION = '1.0.5';
+const APP_VERSION = '1.0.6';
 
 interface Marchio {
   id: number;
